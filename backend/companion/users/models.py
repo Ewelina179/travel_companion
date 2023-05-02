d8ba3984a74c671da78f4ebd617f482cfe69b774
Ewelina.179 <ewelinadalek@gmail.com>
2023-05-02 11:00:32 +0200

    added_models

--- backend/companion/users/models.py
+++ backend/companion/users/models.py
@@ -5,3 +5,17 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+class PersonalData(models.Model):
+    profile = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.CharField(max_length=500)
+    hobbies = models.CharField(max_length=200)
+    languages = models.CharField(max_length=200)
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
